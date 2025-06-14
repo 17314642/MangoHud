1885cc9416733605ed468b041f4bf8679f537207
17314642 <48242771@protonmail.ch>
2025-06-14 21:05:32 +0300

    add server connection

---
+++ src/server_connection.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <mutex>
+#include <set>
+#include "../../MangoHud-2/common/gpu_metrics.hpp"
+
+bool get_active_gpu(const mangohud_message& msg, uint8_t& out_idx);
+std::set<uint8_t> selected_gpus(const mangohud_message& msg);
+void setup_connection_to_server();
+
+extern std::mutex g_metrics_lock;
+extern mangohud_message g_metrics;
